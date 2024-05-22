ec50397eb00c36729f54b65808f6aab8d53b3f80
Ibrahim Azeez <ibrahim_adedamola139@yahoo.com>
2024-05-22 15:26:08 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold  text-red-500 underline">
-        Hello world!
+      <h1 className="text-3xl font-bold justify-center items-center  text-red-500 underline">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Alias voluptas ex eos iure optio deserunt nisi cum, <br /> aut accusamus suscipit!
       </h1>
     </div>
   );
