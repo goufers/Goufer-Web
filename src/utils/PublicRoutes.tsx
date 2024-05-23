934aae285427826d358b9e23b8a0a24cf57d8a67
piustagbas <ptagbajumi12@gmail.com>
2024-05-23 20:49:39 +0100

    added util package

---
+++ src/utils/PublicRoutes.tsx
@@ -0,0 +1,30 @@
+const PUBLIC_ROUTES = {
+
+    LANDINGPAGE: "/landing_page",
+
+    //NAV BAR
+    ABOUT_US:"about/",
+    CONTACT_US:"/contact",
+    CATEGORIES:"/categories",
+
+    LOGIN:"/login",
+    SIGNUP:"/signup",
+    FIND_GOFER:"/find_gofer",
+
+
+    //HERO SECTION
+    SEARCH:"/search",
+
+     //ADMIN ACESS
+     ADMIN:"/admin/login",
+
+
+   // CONSULTATION PAGE: FOR PAYMENTS AND CUSTOMER SERVICE
+    TERMS_AND_CONDITIONS:"/terms_and_conditions",
+    CUSTOMER_CARE:"/customer_care",
+    PAYMENT_METHODS:"/payment_methods",
+
+   
+};
+
+export default PUBLIC_ROUTES;
---
+++ src/utils/pub.urls.tsx
@@ -0,0 +1,13 @@
+
+
+
+const PUUBLIC_URLS = {
+    X_Socials:"https://x.com/",
+    Facebook:"https://www.facebook.com/",
+    Instagram:"https://www.instagram.com/",
+    Linkedin:"https://www.linkedin.com/"
+};
+
+
+export default PUUBLIC_URLS;
+
