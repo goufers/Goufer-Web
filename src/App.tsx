56e3cd506f2ebe76d81adf7fc5ad465c9f01891b
Ibrahim Azeez <ibrahim_adedamola139@yahoo.com>
2024-05-23 11:43:24 +0100

    addition of 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Page404 from "./pages/Page404";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index path="/" element={<Landing />} />
-
-        {/* <Route path={"*"} element={<Page404 />} /> */}
+        <Route path={"*"} element={<Page404 />} />
       </Routes>
     </>
   );
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,12 @@
+
+
+const Page404 = () => {
+  return (
+    <div className='w-full h-screen bg-white flex flex-col '>
+        <img src='./images/error-page-svgrepo-com.svg' className="mx-auto mt-20  w-52 h-52 " alt="404page" />
+        <h1 className="text-3xl text-center mt-4 ">We can't seem to find the page <br />you are looking for</h1>
+    </div>
+  )
+}
+
+export default Page404
