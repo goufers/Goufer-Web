21cc4fbcf2f7887b2c5646870449fe570fc8233f
Ibrahim Azeez <ibrahim_adedamola139@yahoo.com>
2024-05-24 09:08:49 +0100

    addition of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Page404 from "./pages/Page404";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Faq from "./pages/Faq/Faq";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route index path="/" element={<Landing />} />
         <Route path={"*"} element={<Page404 />} />
+        <Route path={"AboutUs"} element={<AboutUs/>}/>
+        <Route path={"Faq"} element={<Faq/>}/>
+        <Route path={"Home"} element={<Home/>} />
       </Routes>
     </>
   );
