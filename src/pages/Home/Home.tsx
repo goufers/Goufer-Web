66a3415f5e884da240d142f39f250b23d68b6891
zucm709 <zuckm709@gmail.com>
2024-05-25 13:56:50 +0100

    json server

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
 
+
+
+interface DbItem {
+  firstName: string;
+
+}
 function Home() {
+  const [dbData, setDbData] = useState<DbItem[]>([]);
+
+  const getDb = async () => {
+         const uri = "http://localhost:3000/users"
+         const res = await fetch(uri);
+         const data: DbItem[] = await res.json()
+         setDbData(data)
+  }
+  getDb()
+
   return (
-    <div>Home</div>
+    <div>
+    {dbData.map((item, index) => (
+      <div key={index}>
+        <p>{item.firstName}</p>
+      </div>
+    ))}
+  </div>
   )
 }
 
