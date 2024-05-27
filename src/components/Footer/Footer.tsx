4a4f23576bdd3f36fe67af38246937ead24899c1
zucm709 <zuckm709@gmail.com>
2024-05-27 14:11:06 +0100

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
-import { faCalendar, faCopyright } from '@fortawesome/fontawesome-free-solid'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import { faCopyright } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+
+
 
 const Footer = () => {
   return (
@@ -11,7 +14,7 @@ const Footer = () => {
       <div className='flex justify-center  gap-28 mt-10' >
             <div className='w-96 mb-5 '>
             <img src="/images/goufer.png" alt="goufer" className='mb-3'/>
-                <p className='text-sm text-gray-500 font-mono'>
+                <p className='text-sm text-gray-500 font-Robotto'>
                 Goufer is a service that bridges the gap between experienced
                 service providers and individuals or businesses in need of their expertise.
                   The name is inspired by "gofer," implying a dependable and resourceful assistant who
@@ -28,46 +31,46 @@ const Footer = () => {
             </div>
             <div>
               <h3 className='font-bold text-gray-500 font-serif mb-3'>Product</h3>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Overview</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Features</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Overview</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Features</li>
               <div className='flex gap-2 items-center'>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Solution</li>
-              <li className='list-none text-xs font-mono text-gray-700  bg-green-200 rounded px-1.5'>New</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Solution</li>
+              <li className='list-none text-xs font-Robotto text-gray-700  bg-green-200 rounded px-1.5'>New</li>
               </div>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Tutorial</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Prices</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Releases</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Tutorial</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Prices</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Releases</li>
             </div>
             <div>
               <h3 className='font-bold text-gray-500 font-serif mb-3'>Company</h3>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>About Us</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>News</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Faqs</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Contact Us</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>About Us</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>News</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Faqs</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Contact Us</li>
             </div>
             <div>
               <h3 className='font-bold text-gray-500 font-serif mb-3'>Resources</h3>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Blog</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Newsletter</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Events</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Help Center</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Tutorials</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Support</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Blog</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Newsletter</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Events</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Help Center</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Tutorials</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Support</li>
             </div>
             <div>
               <h3 className='font-bold text-gray-500 font-serif mb-3'>Legal</h3>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Terms</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Privacy</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Cookies</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Licenses</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Settings</li>
-              <li className='list-none text-sm font-mono text-gray-700 font-bold'>Contact</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Terms</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Privacy</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Cookies</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Licenses</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Settings</li>
+              <li className='list-none text-sm font-Robotto text-gray-700 font-bold px-0.5'>Contact</li>
             </div>
         </div>
         <hr />
         <div className='flex items-center justify-center gap-1 mt-2' >
-          <FontAwesomeIcon icon={faCopyright} style={{width:"15px"}} className='mb-1 text-gray-500'/>
-          <p className=' text-gray-500 font-mono'> 2024 Goufer All Right Reserved.</p>
+         <FontAwesomeIcon icon={faCopyright} className='mb-1 text-gray-500' style={{width:'20px'}}/>
+          <p className=' text-gray-500 font-mono'>2024 Goufer All Right Reserved.</p>
         </div>
       
     </div>
