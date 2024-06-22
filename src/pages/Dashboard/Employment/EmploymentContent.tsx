91e9436d83cb35fed7a4e4f0a73a64f9f3350411
piustagbas <ptagbajumi12@gmail.com>
2024-06-22 22:31:34 +0100

    Employment features

--- src/pages/Dashboard/Employment/EmploymentContent.tsx
+++ src/pages/Dashboard/Employment/EmploymentContent.tsx
@@ -1,9 +1,39 @@
+
+import Employmentdata from "./Employmentdata";
+
+
+
+
 const EmploymentContent = () => {
+
+
   return (
     <div className="w-3/4 h-screen   mt-20">
-      <h3 className="text-3xl text-white p-10 bg-red-700">page content </h3>
-    </div>
+       <div className="flew flex-col p-4 justify-center items-center">
+
+        <h1 className="font-[500] text-[24px]">My Employments</h1>
+
+
+   
+
+    <Employmentdata/>
+
+
+
+  
+
+  
+
+       </div>
+      
+
+  
+
+      </div>
+      
+
   );
 };
 
 export default EmploymentContent;
+
