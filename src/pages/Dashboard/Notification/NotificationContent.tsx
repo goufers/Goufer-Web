28df3f49cdb1e96b6217310665f651d1b222b693
Joseph Samuel <mrsamd02@gmail.com>
2024-07-04 10:55:45 +0100

    .

--- src/pages/Dashboard/Notification/NotificationContent.tsx
+++ src/pages/Dashboard/Notification/NotificationContent.tsx
@@ -1,7 +1,7 @@
 const NotificationContent = () => {
   return (
     <div className="w-3/4 h-screen mt-16 ">
-      <p className="py-10 bg-red-500"></p>
+      <p className="py-10 bg-red-500">Notification</p>
     </div>
   );
 };
